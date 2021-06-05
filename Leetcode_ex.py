117737681fbab2320a86ef478fc82c83d3bd0df9
HeyangLiu <hliu50@horizon.csueastbay.edu>
2021-06-05 00:24:15 -0700

    add q1

--- Leetcode_ex.py
+++ Leetcode_ex.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 def check(s):
     count = 0
     vowel = ['a', 'e', 'i', 'o', 'u']
@@ -7,3 +10,15 @@ def check(s):
     return count
 
 print(check("hourly new day"))
+
+# 1.Two Sum
+def twoSum(nums: List[int], target: int):
+    l = []
+    for i in range(len(nums)-1):
+        for j in range(i+1, len(nums)):
+            if(nums[i]+nums[j]==target):
+                l=[i,j]
+                break
+    return l
+
+
